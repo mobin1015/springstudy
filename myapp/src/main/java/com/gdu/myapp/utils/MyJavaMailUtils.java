b6d6184882f19d30a018f598990d0093918e7525
mobin15 <mobin15@naver.com>
2024-04-01 15:59:23 +0900

    20240401

--- myapp/src/main/java/com/gdu/myapp/utils/MyJavaMailUtils.java
+++ myapp/src/main/java/com/gdu/myapp/utils/MyJavaMailUtils.java
@@ -10,7 +10,16 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@PropertySource(value = "classpath:email.properties")
 public class MyJavaMailUtils {
+  
+  
+  @Autowired
+  private Environment env;
   
   public void sendMail(String to, String subject, String content) {
     
@@ -26,14 +35,18 @@ public class MyJavaMailUtils {
     Session session = Session.getInstance(props, new Authenticator() {
       @Override
       protected PasswordAuthentication getPasswordAuthentication() {
-        return new PasswordAuthentication("gmail", "password");
+        return new PasswordAuthentication(env.getProperty("spring.mail.username")
+                                        ,env.getProperty("spring.mail.password=1234"));
       }
     }); 
-   
+    
+    
     try {
+      
+      
       // 메일 만들기 (보내는 사람 + 받는 사람 + 제목 + 내용) 
       MimeMessage mimeMessage = new MimeMessage(session);
-      mimeMessage.setFrom(new InternetAddress("gmail", "myapp"));
+      mimeMessage.setFrom(new InternetAddress("spring.mail.username", "myapp"));
       mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
       mimeMessage.setSubject(subject);
       mimeMessage.setContent(content, "text/html; charset=UTF-8");
